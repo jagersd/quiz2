ba11bf7c25271a51ace219be88c19aeffbdbeee0
jagersd <jagersd@gmail.com>
2022-07-05 22:27:52 +0200

    quick waiting room tweaks

--- ui/static/waitingroom.js
+++ ui/static/waitingroom.js
@@ -1,6 +1,7 @@
 let quizSlug = document.getElementById("quiz-slug").innerText
 let playerSlug = document.getElementById("player-slug").innerText
 let quizStarted = false
+let ul = document.getElementById("player-list")
 
 let quizState = setInterval(()=> {
 
@@ -8,19 +9,22 @@ let quizState = setInterval(()=> {
     
     fetch(`http://localhost:3000/waitingroom/${quizSlug}/${playerSlug}`,
     {
-    	method: "GET",
-        headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        },
+      method: "GET",
+      headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      },
     })
     .then((response) => response.json())
     .then((responseData) => {
       quizStarted = responseData.quiz.Started;
-      console.log(quizStarted)
+      createItems(responseData.players)
     })
-    .then(quizStarted => quizStarted==true ? clearInterval(quizState) : null)
     .catch(error => console.warn(error));
 }, 2000)
 
 
+function createItems(players) {
+  ul.innerHTML = "";
+  players.forEach(player => ul.innerHTML += `<li>${player}</>`);
+}
