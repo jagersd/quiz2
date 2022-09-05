fe544ffff482020081e7438e3d21c5eaaa6a37f7
jagersd <jagersd@gmail.com>
2022-09-05 21:57:25 +0200

    Disable submit button when no answer provided

--- ui/static/participantroutine.js
+++ ui/static/participantroutine.js
@@ -4,10 +4,10 @@ const quizSession = {
     waitMessage: document.getElementById("waiting-message")
 }
 
-let callCounter = 0;
+let callCounter = 0
 
 let liveResults = setInterval(()=> {
-    callCounter > 1000 ? clearInterval(liveResults) : callCounter += 1;
+    callCounter > 1000 ? clearInterval(liveResults) : callCounter += 1
     
     fetch(`/readytoreveal/${quizSession.quizId}/${quizSession.stage}`,
     {
@@ -22,14 +22,48 @@ let liveResults = setInterval(()=> {
       console.log(responseData)
       parseResults(responseData.result)
     })
-    .catch(error => console.warn(error));
-}, 2000)
+    .catch(error => console.warn(error))
+}, 4000)
 
 function parseResults(result) {
     if (result == true){
-        quizSession.waitMessage.style.display = "none";
-        document.getElementById("options-form").style.display = "block";
+        quizSession.waitMessage.style.display = "none"
+        document.getElementById("options-form").style.display = "block"
     }
 
 }
-  
+
+let submitBtn = document.querySelector('[type=submit]')
+
+let radios = document.querySelectorAll('input[type=radio]')
+if (radios){
+  let checked = document.querySelectorAll('input[type=radio]:checked')
+
+  if(!checked.length){
+    submitBtn.style.opacity = "0.6"
+    submitBtn.setAttribute("disabled", "disabled")
+  }
+  radios.forEach(function(el){
+    el.addEventListener('click', function(){
+      checked = document.querySelectorAll('input[type=radio]:checked')
+      if(checked.length){
+        submitBtn.style.opacity = "1"
+        submitBtn.removeAttribute("disabled")
+      }
+    })
+  })
+}
+
+let openAnswer = document.querySelector('input[type=text]')
+if (openAnswer){
+  openAnswer.addEventListener('change', function(){
+    if (openAnswer.value == ""){
+      submitBtn.style.opacity = "0.6"
+      submitBtn.setAttribute("disabled", "disabled")
+    } else {
+      submitBtn.style.opacity = "1"
+      submitBtn.removeAttribute("disabled")
+    }
+  })
+}
+
